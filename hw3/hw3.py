7874af52808fdf1eebd7d5b05a5de262c8888c1e
josadhika <jt.adhika.2023@mqf.smu.edu.sg>
2023-12-14 13:29:49 +0800

    formatting

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -25,32 +25,35 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import sklearn.metrics as metrics
 from sklearn.ensemble import RandomForestClassifier
+
 # read data frame
 df = pd.read_csv("/home/kilo/Desktop/qf634/qf634file/banking3.csv")
 df.dropna(inplace=True)
 # print(df.info())
 y = df["y"]
 X = df.drop(columns=["Unnamed: 0", "y"])
-#print(y.info())
-#print(X.info())
+# print(y.info())
+# print(X.info())
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-RF_model = RandomForestClassifier(n_estimators=1000, random_state=1, max_features=None, max_depth=None)
-RF_model.fit(X_train,y_train)
+RF_model = RandomForestClassifier(
+    n_estimators=1000, random_state=1, max_features=None, max_depth=None
+)
+RF_model.fit(X_train, y_train)
 y_pred_RF = RF_model.predict(X_test)
 
 Accuracy_RF = metrics.accuracy_score(y_test, y_pred_RF)
-print("RF Accuracy:",Accuracy_RF)
+print("RF Accuracy:", Accuracy_RF)
 
 confusion_matrix = confusion_matrix(y_test, y_pred_RF)
-print('------- confusion matrix -------')
+print("------- confusion matrix -------")
 print(confusion_matrix)
-print('\n------- classification report -------')
+print("\n------- classification report -------")
 print(classification_report(y_test, y_pred_RF))
 
 # calculate the fpr and tpr for all thresholds of the classification
-preds_RF = RF_model.predict_proba(X_test)[:,1]
-fpr, tpr, thresholds = metrics.roc_curve(y_test, preds_RF)  
+preds_RF = RF_model.predict_proba(X_test)[:, 1]
+fpr, tpr, thresholds = metrics.roc_curve(y_test, preds_RF)
 # matches y_test of 1's and 0's versus pred prob of 1's for each of the 10,523 test cases
 # sklearn.metrics.roc_curve(y_true, y_score,...) requires y_true as 0,1 input and y_score as prob inputs
 # this metrics.roc_curve returns fpr, tpr, thresholds (Decreasing thresholds used to compute fpr and tpr)
@@ -60,7 +63,7 @@ roc_auc_logreg = metrics.auc(fpr, tpr)
 # Compute Area Under the Curve (AUC) using the trapezoidal rule.
 # This is a general function, given points on a curve. For computing the area under the ROC-curve, see roc_auc_score.
 
-#plot stuff
+# plot stuff
 plt.rc("font", size=14)
 plt.title("Receiver Operating Characteristic")
 plt.plot(fpr, tpr, "b", label="AUC = %0.5f" % roc_auc_logreg)
